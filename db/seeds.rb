70db30d00a7d34a89227c4dccae9858dbebb4c29
Kilian Hayat <khayat@citycable.ch>
2021-03-17 12:25:14 +0100

    finished restauran controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+puts "Creating restaurants..."
+
+Pizza = Restaurant.create(name: "Pizza Yolo", address: "in the sky", phone_number: "0423387238234", category: "italian")
+Nouille = Restaurant.create(name: "Chez Xu", address: "in the trainstation", phone_number: "042338723213234", category: "chinese")
+
+puts "Finished!"
